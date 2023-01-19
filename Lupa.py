0af1b6659304f865df77e91d07a8b5f987c5aebf
Eduardo José Barrios García <eduardojbarriosgarcia@gmail.com>
2023-01-19 11:58:16 +0000

    Draw Mouse feature

--- Lupa.py
+++ Lupa.py
@@ -46,6 +46,34 @@ def zoom(zoom_level):
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    
+    def draw_shape(img, shape_type):
+    if shape_type == "circle":
+        # Obtener la posición del cursor
+        mouse = Controller()
+        x, y = mouse.position
+
+        # Dibujar un círculo en la posición del cursor
+        cv2.circle(img, (x, y), 50, (0, 0, 255), -1)
+    elif shape_type == "rectangle":
+        # Obtener la posición del cursor
+        mouse = Controller()
+        x, y = mouse.position
+
+        # Dibujar un rectángulo en la posición del cursor
+        cv2.rectangle(img, (x-50, y-50), (x+50, y+50), (0, 255, 0), -1)
+    elif shape_type == "line":
+        # Obtener la posición del cursor
+        mouse = Controller()
+        x1, y1 = mouse.position
+        print("Press the mouse button again at the end point of the line")
+        with mouse.pressed(Button.left):
+            x2, y2 = mouse.position
+        # Dibujar una línea desde la posición del cursor inicial hasta la posición final
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 5)
+    cv2.imshow("Shape", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 zoom_level = input("Ingrese 'in' para acercar o 'out' para alejar: ")
 zoom(zoom_level)
